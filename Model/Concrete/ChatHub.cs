21b378173b5bbd2dbb5415950696d0ff79669462
Halil Bilen <halil@Halil-Bilen-MacBook.local>
2021-01-25 17:21:31 +0300

    first commit

---
+++ Model/Concrete/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Model.Abstract;
+
+namespace Model.Concrete
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            var date = DateTime.Now;
+            await Clients.All.SendAsync("ReceiveMessage", user, message,date);
+        }
+    }
+}
---
+++ WebSocket/Controllers/SocketController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebSocket.Controllers
+{
+    [Produces("application/json")]
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class SocketController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Index(int id)
+        {
+            var response = id;
+            return Ok(response);
+        }
+    }
+}
